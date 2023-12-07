5de39fae12a1f8a930c5b19d04dd042b31cc0bb1
RaiUmerFarooq <RaiUmerFarooq>
2023-12-08 01:51:37 +0500

    added new jobs

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def main():
     print("Welcome to the Rectangle Calculator!")
 
     # Get user input for length and width
-    length = float(input("Enter the length of the rectangle: "))
-    width = float(input("Enter the width of the rectangle: "))
+    length = 12
+    width = 13
 
     # Calculate area and perimeter
     area, perimeter = calculate_rectangle_properties(length, width)
