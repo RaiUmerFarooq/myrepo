27e027a59cbfd786e219b875905ecf2601f4efa5
RaiUmerFarooq <RaiUmerFarooq>
2023-12-08 01:43:59 +0500

    added new jobs

--- test.py
+++ test.py
@@ -1,17 +1,23 @@
 import unittest
-from calculator import add, subtract
+from main import calculate_rectangle_properties
 
-class TestCalculator(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(2, 3), 5)
-        self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(-1, -1), -2)
+class TestRectangleCalculator(unittest.TestCase):
+    def test_calculate_rectangle_properties(self):
+        # Test with positive integers
+        result = calculate_rectangle_properties(5, 3)
+        self.assertEqual(result, (15, 16))
 
-    def test_subtract(self):
-        self.assertEqual(subtract(5, 2), 3)
-        self.assertEqual(subtract(-1, 1), -2)
-        self.assertEqual(subtract(-1, -1), 0)
+        # Test with floating-point numbers
+        result = calculate_rectangle_properties(2.5, 4.5)
+        self.assertEqual(result, (11.25, 14))
+
+        # Test with zero
+        result = calculate_rectangle_properties(0, 10)
+        self.assertEqual(result, (0, 20))
+
+        # Test with negative numbers
+        result = calculate_rectangle_properties(-2, 8)
+        self.assertEqual(result, (-16, 12))
 
 if __name__ == '__main__':
     unittest.main()
-
