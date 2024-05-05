9615f1dde1a0474b887a716a3bc7a2b31f7b8fe5
Rai Umer <raifarooq7860786@gmail.com>
2024-05-06 02:11:24 +0500

    h1

---
+++ index.js
@@ -0,0 +1,16 @@
+function check(){
+    const v1=document.getElementById("firstname").value;
+    const v2=document.getElementById("lastname").value;
+   if(v1==="nini" || v1==="Nini"){
+if(v2==="umzy" || v2==="Umzy"){
+alert("goodjob")
+window.location="index1.html"
+}
+else{
+    alert("Write the Second Name which You Love To Call Me")
+}
+   }
+   else{
+    alert("write the name which i love to call you")
+   }
+}
